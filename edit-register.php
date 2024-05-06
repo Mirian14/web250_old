95bd44715b12a0093ef918393155540cdfc6f4b8
Mirian14 <148568149+Mirian14@users.noreply.github.com>
2024-05-05 23:13:47 -0400

    edit-register.php

---
+++ edit-register.php
@@ -0,0 +1,15 @@
+<main>
+<div class="divintro container-contract">
+        <div class="tittle-contract">
+            <div class="container-main">
+                <h2>Services</h2>
+            </div>
+        </div>
+        <div class='container-login'>            
+            <fieldset class="list">  
+                <legend>Edit detail service </legend>             
+                
+            </fieldset>
+        </div>
+    </div>
+</main>
